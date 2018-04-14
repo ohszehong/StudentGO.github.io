a6f4923f8cc47506ceaf9c1d06b67cf9ccdbf590
tweijiunn1999 <tweijiunn@gmail.com>
2018-04-14 23:42:48 +0800

    Added Java

---
+++ StudentFood/GenerateCouponCode.js
@@ -0,0 +1,12 @@
+﻿function Generate(length){
+ var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (var i = 0; i < possible.length; i++){
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  }
+  document.getElementById("generateCode").value= text;
+  
+  }
+  
